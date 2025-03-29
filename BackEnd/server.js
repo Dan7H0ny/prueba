d62653ba7c60aac5c53ace6fd04fd23dc9af421e
unknown <loqendo759@gmail.com>
2025-03-29 17:47:11 -0400

    subida inicial

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -33,7 +33,7 @@ app.post('/api/login', async (req, res) => {
     await user.save();
     res.status(200).json({ message: 'Datos guardados' });
   } catch (err) {
-    res.status(500).json({ error: 'Error al guardar datos' });
+    res.status(500).json({ error: 'Error al guardar datos', err});
   }
 });
 
